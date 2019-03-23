f3ce46bfb97fd3c061edeb1d78a19448bd90235a
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-23 13:07:29 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -28,9 +28,10 @@ end
 
 def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
-  while counter <= number_of_times do
+  counter = 0
+  while counter < number_of_times do
     puts phrase
+    counter += 1
   end
 end
 
